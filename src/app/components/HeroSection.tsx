05417ae799fe3a35a7d3bbd1db5b97fdd940f0cb
Shyju Viswambaran <shyjuezy@gmail.com>
2024-12-20 21:55:55 -0600

    Final change

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
-import { Gradient } from '@/components/gradient'
+// import { Gradient } from '@/components/gradient'
 import Link from 'next/link'
 
 export function HeroSection() {
